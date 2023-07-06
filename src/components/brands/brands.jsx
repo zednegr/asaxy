d774cf5a9c73c5d541752d18901abb50b7262fd2
Asatillo-2005 <gsatoro517@gmail.com>
2023-07-06 12:24:43 +0300

    ok

---
+++ src/components/brands/brands.jsx
@@ -0,0 +1,16 @@
+import "./brands.scss"
+
+
+function Brands(){
+    return(
+        <section className="section-5">
+            <div className="container">
+                <div className="brands-box">
+                    <div className="brands-item">
+                        
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
