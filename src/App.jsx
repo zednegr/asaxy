c127eeb19648d33b42cd0e47c44e97ccbfa76319
Asatillo-2005 <gsatoro517@gmail.com>
2023-07-06 15:11:05 +0300

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Header from './components/header/header.jsx'
 import Brands from './components/brands/brands'
 import Apps from './components/apps/apps'
 import Section from './components/section/section'
+import Hero from './components/hero/hero'
 
 function App() {
 
   return (
     <>
     <Header />
+    <Hero/>
     <Brands/>
     <Apps/>
     <Section/>
