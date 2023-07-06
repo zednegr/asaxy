166e49cc22ab69516290619889b62ddd9191594f
zednegr <negrzed@gmail.com>
2023-07-06 10:37:18 +0500

    zed edited

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-       <h1>Salom Bolar</h1>
-       <div className="container">
-          Lorem ipsum dolor sit amet.
-       </div>
-      <p>Asatillo </p>
+       
     </>
   )
 }
