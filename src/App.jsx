6e1a9d1be6906d1ab8954f0a5a7bd203bbd37d63
Asatillo-2005 <gsatoro517@gmail.com>
2023-07-06 14:48:23 +0300

    edition done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css'
 import Footer from './components/footer/footer'
 import Header from './components/header/header.jsx'
+import Brands from './components/brands/brands'
+import Apps from './components/apps/apps'
+import Section from './components/section/section'
 
 function App() {
 
   return (
     <>
     <Header />
+    <Brands/>
+    <Apps/>
+    <Section/>
     <Footer />
     </>
    
--- src/components/brands/brands.jsx
+++ src/components/brands/brands.jsx
@@ -1,16 +1,67 @@
 import "./brands.scss"
 
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-function Brands(){
-    return(
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+// import required modules
+import { Pagination } from 'swiper/modules';
+
+
+
+function Brands() {
+    return (
         <section className="section-5">
             <div className="container">
                 <div className="brands-box">
-                    <div className="brands-item">
+                        <Swiper
+                            slidesPerView={1}
+                            spaceBetween={10}
+                            pagination={{
+                                clickable: true,    
+                            }}
+                            breakpoints={{
+                                '@0.00': {
+                                    slidesPerView: 1,
+                                    spaceBetween: 10,
+                                },
+                                '@0.75': {
+                                    slidesPerView: 2,
+                                    spaceBetween: 20,
+                                },
+                                '@1.00': {
+                                    slidesPerView: 3,
+                                    spaceBetween: 40,
+                                },
+                                '@1.50': {
+                                    slidesPerView: 4,
+                                    spaceBetween: 50,
+                                },
+                            }}
+                            modules={[Pagination]}
+                            className="mySwiper"
+                        >
+                        <SwiperSlide><a className="swiper-a" href="#"><img src="https://assets.asaxiy.uz/brand/webp//5e15c4ec85cf2.webp" alt="error" /></a></SwiperSlide>
+                        <SwiperSlide><a className="swiper-a" href="#"><img src="https://assets.asaxiy.uz/brand/webp//5e15c5de44b63.webp" alt="error" /></a></SwiperSlide>
+                        <SwiperSlide><a className="swiper-a" href="#"><img src="https://assets.asaxiy.uz/brand/webp//5e15c615d99f8.webp" alt="error" /></a></SwiperSlide>
+                        <SwiperSlide><a className="swiper-a" href="#"><img src="https://assets.asaxiy.uz/brand/webp//5e15c63e06b95.webp" alt="error" /></a></SwiperSlide>
+                        <SwiperSlide><a className="swiper-a" href="#"><img src="https://assets.asaxiy.uz/brand/webp//5e15c561deadb.webp" alt="error" /></a></SwiperSlide>
+                        <SwiperSlide><a className="swiper-a" href="#"><img src="https://assets.asaxiy.uz/brand/webp//5e15c4697e437.webp" alt="error" /></a></SwiperSlide>
+                        <SwiperSlide><a className="swiper-a" href="#"><img src="https://assets.asaxiy.uz/brand/webp//5e15c51449bd0.webp" alt="error" /></a></SwiperSlide>
+                        <SwiperSlide><a className="swiper-a" href="#"><img src="https://assets.asaxiy.uz/brand/webp//5ff6f8872a0db.webp" alt="error" /></a></SwiperSlide>
+                        <SwiperSlide><a className="swiper-a" href="#"><img src="https://assets.asaxiy.uz/brand/webp//603481675b122.webp" alt="error" /></a></SwiperSlide>
+
                         
-                    </div>
+                    
+                        </Swiper>
                 </div>
             </div>
         </section>
-    )
+    )   
 }
+
+export default Brands
