e8837b246e69c64423de18574ab17483ed796590
Asatillo-2005 <gsatoro517@gmail.com>
2023-07-07 08:28:47 +0300

    done

--- src/components/section/section.jsx
+++ src/components/section/section.jsx
@@ -8,23 +8,23 @@ function Section() {
                     <ul className="section-link">
                         <li className="section-links">
                             <h3 className="section-links-title">Больше не нужно ходить на базар</h3>
-                                <p className="section-links-text">У нас выгодные цены и
-                                    доставка до дома</p>
+                            <p className="section-links-text">У нас выгодные цены и
+                                доставка до дома</p>
                         </li>
                         <li className="section-links">
                             <h3 className="section-links-title">Быстрая доставка</h3>
-                                <p className="section-links-text">Наш сервис удивит вас</p>
+                            <p className="section-links-text">Наш сервис удивит вас</p>
                         </li>
                         <li className="section-links">
                             <h3 className="section-links-title">Удобства для вас</h3>
-                                <p className="section-links-text">Быстрое оформление и
-                                    гарантия на возврат в
-                                    случае неисправности</p>
+                            <p className="section-links-text">Быстрое оформление и
+                                гарантия на возврат в
+                                случае неисправности</p>
                         </li>
                         <li className="section-links">
                             <h3 className="section-links-title">Рассрочка</h3>
-                                <p className="section-links-text">Без предоплаты на 6 или
-                                    12 месяцев</p>
+                            <p className="section-links-text">Без предоплаты на 6 или
+                                12 месяцев</p>
                         </li>
                     </ul>
                 </div>
