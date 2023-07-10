4ae202efe519af832a3690c2de84963c657972ce
rustamov206 <rustamovazamxon314@gmail.com>
2023-07-10 10:12:55 +0500

    salom

---
+++ src/components/cards/card-inner/card-inner.jsx
@@ -0,0 +1,15 @@
+import './card-inner.scss'
+
+function InnerCard() {
+    return (
+        <>
+        <div className="hero-2">
+            <div className="content-inner">
+                
+            </div>
+        </div>
+        
+        </>
+    )
+}
+export default InnerCard
