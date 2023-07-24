9640a77d091d849c1988ee5fffbb84664e9486b7
zednegr <negrzed@gmail.com>
2023-07-24 13:43:40 +0500

    first update zed!

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,10 +1,11 @@
 import './hero.scss'
+import HeroSale from "../../assets/img/hero-sale.png"
 import React, { useRef, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
-import { Pagination } from 'swiper/modules';
-import HeroSale from "../../assets/img/hero-sale.png"
+import 'swiper/css/navigation';
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 
 function Hero() {
     return (
@@ -14,10 +15,18 @@ function Hero() {
                     <div className="hero-slider">
 
                         <Swiper
-                            pagination={{
-                                dynamicBullets: true,
+                            slidesPerView={1}
+                            spaceBetween={30}
+                            loop={true}
+                            centeredSlides={true}
+                            autoplay={{
+                                delay: 1500,
+                                disableOnInteraction: false,
                             }}
-                            modules={[Pagination]}
+                            pagination={{
+                                clickable: true,
+                            }}
+                            modules={[Autoplay, Pagination]}
                             className="hero_swiper"
                         >
                             <SwiperSlide className='hero-slide'><img src="https://assets.asaxiy.uz/uploads/banner/desktop/64a25f5857261.jpg.webp" alt="error" /></SwiperSlide>
@@ -26,7 +35,7 @@ function Hero() {
                             <SwiperSlide className='hero-slide'><img src="https://assets.asaxiy.uz/uploads/banner/desktop/6454e5f4a7457.jpg.webp" alt="error" /></SwiperSlide>
                             <SwiperSlide className='hero-slide'><img src="https://assets.asaxiy.uz/uploads/banner/desktop/64748df45849f.jpg.webp" alt="error" /></SwiperSlide>
                         </Swiper>
-                        
+
                         <div className="hero-sale">
                             <img src={HeroSale} alt="error" />
                         </div>
